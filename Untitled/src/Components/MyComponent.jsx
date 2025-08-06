a7fd4d0438d798aea11952460573c95e12f30311
Thato Hopolang Botsane <thbjr@outlook.com>
2025-08-06 08:03:02 +0200

    Moves! MyComponent.jsx to Components

--- Untitled/src/Components/MyComponent.jsx
+++ Untitled/src/Components/MyComponent.jsx
@@ -5,6 +5,8 @@ function MyComponent() {
 
     const [name, setName] = useState("")
     const [comment, setComment] = useState("")
+    const [payment, setPayment] = useState("")
+    const [shipping, setShipping] = useState("")
 
     function handleNameChange(event) {
         setName(event.target.value)
@@ -12,6 +14,14 @@ function MyComponent() {
 
     function handleCommentChange(event) {
         setComment(event.target.value)
+    }
+    
+    function handlePaymentChange(event) {
+        setPayment(event.target.value)
+    }
+
+    function handleShippingChange(event) {
+        setShipping(event.target.value)
     }
 
     return(
@@ -21,6 +31,27 @@ function MyComponent() {
 
             <p>Delivery Instructions</p>
             <textarea value={comment} onChange={handleCommentChange} placeholder="Leave additional Delivery Instructions"/>
+
+            <p>Payment Option</p>
+            <select value={payment} onChange={handlePaymentChange}>
+                <option value="">Select Payment</option>
+                <option value="Visa">Visa</option>
+                <option value="MasterCard">MasterCard</option>
+            </select>
+
+            <div className="div">
+                <label>
+                    <input type="radio" value="Pick Up" checked={shipping === "Pick Up"} onChange={handleShippingChange}/>
+                    Pick Up
+                </label><br/>
+
+                <label>
+                    <input type="radio" value="Delivery" checked={shipping === "Delivery"} onChange={handleShippingChange}/>
+                    Delivery
+                </label>
+
+                <p>Delivery Method: {shipping}</p>
+            </div>
 
 
         </div>
